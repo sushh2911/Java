ffb4a4518a76c13f68b52ff1f7b501e79962d40b
sushh2911 <sushant.aggarwal2021@vitbhopal.ac.in>
2023-06-19 23:51:27 +0530

    second

---
+++ Basics of Java/subtraction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+class subtraction{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter any two numbers!");
+
+        a = s.nextInt();
+        b = s.nextInt();
+        c = a-b;
+        System.out.println("the subtraction is "+c);
+        
+    }
+}
